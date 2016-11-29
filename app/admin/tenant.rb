52875a24484427a6c0e6066cda7b73757f72c171
Raghu Betina <raghu@firstdraft.com>
2016-11-29 16:42:50 +0000

    Whitelist columns in Tenant for ActiveAdmin

--- app/admin/tenant.rb
+++ app/admin/tenant.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Tenant do
 
+ permit_params :email, :password
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
