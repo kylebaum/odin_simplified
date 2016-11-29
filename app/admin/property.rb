e1ce90e7cb0a974cec1c505b248fc99bee2f4054
Raghu Betina <raghu@firstdraft.com>
2016-11-29 16:42:38 +0000

    Whitelist columns in Property for ActiveAdmin

--- app/admin/property.rb
+++ app/admin/property.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Property do
 
+ permit_params :owner_id, :tenant_id, :manager_id, :home_or_apartment, :number_bedrooms, :number_bathrooms, :city, :neighborhood, :policies, :price
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
