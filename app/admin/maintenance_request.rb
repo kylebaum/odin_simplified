cda8b308c1409ac8319cdb3eb95ce95a5193267e
Raghu Betina <raghu@firstdraft.com>
2016-11-29 16:42:58 +0000

    Whitelist columns in MaintenanceRequest for ActiveAdmin

--- app/admin/maintenance_request.rb
+++ app/admin/maintenance_request.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register MaintenanceRequest do
 
+ permit_params :property_id, :body_of_request
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
